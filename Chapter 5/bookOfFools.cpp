b730beb142b72b0c2e7c5ca3e9e51ad2cff8aae5
Pinheiro <rodrigo.pinheiro@sap.com>
2017-10-05 17:00:12 -0300

    book of fools with another version commented

--- Chapter 5/bookOfFools.cpp
+++ Chapter 5/bookOfFools.cpp
@@ -34,3 +34,31 @@ int main(){
 
     return 0;
 }
+
+/*
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+ 
+int main()
+{
+string month[12] = {"January","February ","March ","April ","May","June","July","August","September","October","November","December"};
+int sales[12];
+int sum = 0;
+ 
+cout << "Enter sales for the month: " << endl;
+ 
+for(int i = 0; i < 12; i++)
+{
+cout << month[i] << ": ";
+cin >> sales[i];
+sum=sum+sales[i];
+};
+ 
+cout << "Total sales for the year: " << sum << endl;
+ 
+return 0;
+} 
+
+*/
